ced18538e2b0c461981cc91ef82546374adefa66
Serhii Mamchur <kovaniysvit@gmail.com>
2025-10-28 12:39:29 +0100

    Solution2

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,33 +1,40 @@
 type Shape = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
 
-
 export interface Figure {
-  shape: Shape,
-  color: Color,
-  getArea(): number,
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
+
+function floorToHundredths(value: number): number {
+  return Math.floor(value * 100) / 100;
 }
 
 export class Triangle implements Figure {
   constructor(
     public color: Color,
-    public readonly shape: Shape = 'triangle',
     public a: number,
     public b: number,
     public c: number,
+    public readonly shape: Shape = 'triangle',
   ) {
     const longestSide = Math.max(a, b, c);
     const sumOfTheOtherTwo = a + b + c - longestSide;
 
-    if(longestSide >= sumOfTheOtherTwo || Math.min(a, b, c) <= 0) {
+    if (longestSide >= sumOfTheOtherTwo || Math.min(a, b, c) <= 0) {
       throw new Error('longestSide has to be lower than sumOfTheOtherTwo');
     }
   }
 
   getArea(): number {
     const perimeter = (this.a + this.b + this.c) / 2;
-    const triangleArea =
-    Math.sqrt(perimeter * (perimeter - this.a) * (perimeter - this.b) * (perimeter - this.c));
+    const triangleArea = Math.sqrt(
+      perimeter *
+        (perimeter - this.a) *
+        (perimeter - this.b) *
+        (perimeter - this.c),
+    );
 
     return floorToHundredths(triangleArea);
   }
@@ -36,8 +43,8 @@ export class Triangle implements Figure {
 export class Circle implements Figure {
   constructor(
     public color: Color,
-    public readonly shape: Shape = 'circle',
     public radius: number,
+    public readonly shape: Shape = 'circle',
   ) {
     if (radius <= 0) {
       throw new Error('radius must be higher than 0');
@@ -45,7 +52,7 @@ export class Circle implements Figure {
   }
 
   getArea(): number {
-   return floorToHundredths(this.radius * this.radius * Math.PI);
+    return floorToHundredths(this.radius * this.radius * Math.PI);
   }
 }
 
@@ -56,24 +63,19 @@ export class Rectangle implements Figure {
     public height: number,
     public readonly shape: Shape = 'rectangle',
   ) {
-
-   if (this.width <= 0 || this.height <= 0) {
-       throw new Error('width or height must be more than 0');
-   }
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('width or height must be more than 0');
+    }
   }
 
-    getArea(): number {
-    return   floorToHundredths(this.height * this.width);
-    }
+  getArea(): number {
+    return floorToHundredths(this.height * this.width);
+  }
 }
 
 export function getInfo(figure: Figure): string {
   // return typeof figure;
   // 'A red rectangle - 15';
 
-  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`
-}
-
-function floorToHundredths(value: number): number {
- return Math.floor((value * 100)) / 100;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
